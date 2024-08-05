e4c8d69337bf1ba260e82f30ee501bbf7cfc9804
eshmaa357 <timsinaishma@gmail.com>
2024-08-05 18:42:22 +0545

    Add files via upload

---
+++ Ishma dot net Lab 1.cs
@@ -0,0 +1,15 @@
+﻿//Write C# console program to take input from user to print Yourname and print "Welcole Yourname, to dotnet technology"
+
+using System;namespace Lab1Qn1
+{
+    class Lab1Qn1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name ?");
+            String name = Console.ReadLine();
+            Console.WriteLine("Welcome " + name + " to dotnet technology");
+            Console.ReadLine();
+        }
+    }
+}
