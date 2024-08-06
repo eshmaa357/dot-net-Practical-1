8c1cdb3b136ab999946fb933e18c5812fe48f413
eshmaa357 <timsinaishma@gmail.com>
2024-08-06 07:30:01 +0545

    Add files via upload

---
+++ IshmaLab1Qn8.cs
@@ -0,0 +1,26 @@
+﻿//How to reverse a string using a C# program? 
+
+using System;
+namespace Ishma_dot_net_practical_1
+{
+    internal class IshmaLab1Qn8
+    {
+        /*
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter anything: ");
+            String value = Console.ReadLine();
+            String rev = ReverseString(value);
+            Console.WriteLine("Reverse string is : {0}  ", rev );
+
+             String ReverseString(String s)
+            {
+                char[] chars = s.ToCharArray();
+                Array.Reverse(chars);
+                return new String(chars);
+            }
+            Console.ReadLine();
+        }
+        */
+    }
+}
